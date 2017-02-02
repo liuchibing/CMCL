b82bc8ea036c8d9962d92c37fe436b940f9e53fd
liuchibing <charlie428b@hotmail.com>
2017-02-02 22:13:33 +0800

    。

---
+++ src/CMCL.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * Created by charlie on 2017/1/29.
+ */
+public class CMCL {
+
+    private JTabbedPane tabbedPane;
+    private JPanel panelRoot;
+    private JPanel tabVersions;
+    private JPanel tabManage;
+    private JPanel tabSettings;
+    private JPanel tabForge;
+    private JList listInstalled;
+    private JLabel labelInfo;
+    private JButton btnDelete;
+    private JButton btnRename;
+    private JButton btnModManage;
+    private JButton btnModFolder;
+    private JButton btnModSettings;
+    private JButton btnStart;
+    private JTextField inputUsername;
+    private JTextField inputPassword;
+    private JRadioButton radioOffline;
+    private JRadioButton radioAuthorized;
+    private JRadioButton radioBMCLAPI;
+    private JRadioButton radioOfficial;
+    private JTextField inputPath;
+    private JButton 浏览Button;
+    private JTextField inputMemorySize;
+
+}
--- src/CMCLMain.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by charlie on 2017/1/29.
- */
-public class CMCLMain {
-}
